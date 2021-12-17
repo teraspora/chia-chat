34f652dc6cb651c62ab3bfe134f4a90f5d591e5f
John Lynch <johnlynch.32768@gmail.com>
2021-12-17 01:25:55 +0000

    More changes.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,80 @@
+// Let user use up and down arrows to retrieve previous commands/messages
+const COMMAND_SYMBOL = `$`;
+const UP = `ArrowUp`;
+const DOWN = `ArrowDown`;
+const DASH = `-`;
+const BANG = `!`;
+const SPACE = ` `;
+const m = document.getElementById(`m`);
+const msgs = document.getElementById(`messages`);
+const inc = (_ => (i = 0, _ => i++))();
+let mkey = 0;
+let name = `Anon`;
+
 document.onreadystatechange = _ => {
     if (document.readyState === `complete`) {
         var socket = io();
         document.forms[0].onsubmit = ev => { 
             ev.preventDefault(); // prevents page reloading
-            socket.emit(`Chia Chat Message`, document.getElementById(`m`).value);
-            document.getElementById(`m`).value = ``;
+            const msg = m.value;
+            socket.emit(`Chia Chat Message`, msg);
+            localStorage.setItem(mkey = inc(), msg);
+            m.value = ``;
             return false;
         };
         socket.on('Chia Chat Message', msg => {
             const li = document.createElement(`li`);
+            console.log(msg);
+            if (msg.startsWith(COMMAND_SYMBOL)) {
+                process_command(msg.substr(1));
+            }
             const text = document.createTextNode(msg);
             li.appendChild(text);
-              document.getElementById(`messages`).appendChild(li);
+              msgs.appendChild(li);
             });
     }
-  };
+};
+
+window.addEventListener('keydown', ev => {
+    key = ev.key || ev.keyCode;   // keyCode is an older standard
+    switch (key) {
+        case UP:
+            if (mkey > 0) mkey--;
+            m.value = localStorage.getItem(mkey);
+            break;
+        case DOWN:
+            if (localStorage.getItem(mkey + 1)) mkey++;
+            m.value = localStorage.getItem(mkey);
+            break;
+        default:
+            ;
+            break;
+    }        
+});
+
+function process_command(cmd) {
+    console.log(cmd);
+    switch(cmd.substr(0, 1)) {
+        case DASH:
+            console.log(`Colour change command arrived from the ether... `)
+            // Allow for hex values or CSS colour names or rgb() calls
+            setBackground(msgs, cmd.substr(1));
+            break;
+        case BANG:
+            console.log(`Rotation command arrived from the ether... `)
+            rotateBody(cmd.substr(1));
+            break;
+        case SPACE:
+            name = cmd.substr(1).replace(/^\w/, s => s.charAt(0).toUpperCase() + s.substr(1));
+            break;
+        default:
+            break;
+    }
+}
+
+function setBackground(element, colour) {
+    element.style.backgroundColor = colour;
+}
+function rotateBody(phi) {
+    document.body.style.transform = `rotate(${phi})`;
+}
--- server.js
+++ server.js
@@ -2,21 +2,25 @@
 // John Lynch, October 2019
 // Based on tutorial at https://socket.io/get-started/chat/
 
-var express = require(`express`);
-var app = express();
-var http = require(`http`).createServer(app);
-var io = require(`socket.io`)(http);
+const express = require(`express`);
+const app = express();
+const http = require(`http`).createServer(app);
+const io = require(`socket.io`)(http);
+const port = process.env.PORT || 5000;
 
+// scripts/ and styles/ dirs go in here but referenced  without'public'
 app.use(express.static(__dirname + `/public`));
 
+// Root route
 app.get(`/`, (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Handle user connecting
 io.on(`connection`, socket => {
   console.log(`Super.   A user connected!`);
   socket.on(`Chia Chat Message`, msg => {
-    socket.broadcast.emit(`Chia Chat Message`, msg);
+    socket.broadcast.emit(`Chia Chat Message`, msg);  // broadcast sends to all *other* connections
     console.log(`Message: ${msg}`);
   });
   socket.on(`disconnect`, _ => {
@@ -24,6 +28,6 @@ io.on(`connection`, socket => {
   });
 });
 
-http.listen(process.env.PORT || 5000, _ => {
-  console.log(`Chia is listening on port 5000.`);
+http.listen(port, _ => {
+  console.log(`Chia is now listening on port ${port}.`);
 });
